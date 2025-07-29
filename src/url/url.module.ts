384cf39f035329e8b526a6356af3a061b1343f04
Sakshi Agnihotri <sakshiagnihotri292@gmail.com>
2025-07-30 05:22:23 +0530

    Update url.module.ts

--- src/url/url.module.ts
+++ src/url/url.module.ts
@@ -10,19 +10,3 @@ import { Url, UrlSchema } from './schemas/url.schema';
   providers: [UrlService],
 })
 export class UrlModule {}
-
-// import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { UrlService } from './url.service';
-// import { Url, UrlSchema } from './schemas/url.schema';
-// import { ConfigModule } from '@nestjs/config'; // 👈 Add this line
-
-// @Module({
-//   imports: [
-//     MongooseModule.forFeature([{ name: Url.name, schema: UrlSchema }]),
-//     ConfigModule, // 👈 Include this for access to ConfigService
-//   ],
-//   providers: [UrlService],
-//   exports: [UrlService],
-// })
-// export class UrlModule {}
